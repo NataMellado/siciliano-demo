7c503bf9ff0c4d2129cfe492bd5ced06ef183788
NataMellado <natalia.cmc92@gmail.com>
2026-02-20 20:14:18 -0300

    first commit

--- data.js
+++ data.js
@@ -1,8 +1,296 @@
 var APP_DATA = {
   "scenes": [
     {
-      "id": "0-dji_20260219132255_0005_v",
-      "name": "DJI_20260219132255_0005_V",
+      "id": "0-estancia-siciliano",
+      "name": "Estancia Siciliano",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3528,
+      "initialViewParameters": {
+        "yaw": -2.4090014937301127,
+        "pitch": 0.5876974442732426,
+        "fov": 1.6626545459918893
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.726489830305736,
+          "pitch": 1.1276350146471632,
+          "rotation": 0,
+          "target": "3-7a"
+        },
+        {
+          "yaw": -2.237378803042697,
+          "pitch": 1.376994241449145,
+          "rotation": 0,
+          "target": "4-7b"
+        },
+        {
+          "yaw": 2.431197070032473,
+          "pitch": 1.3584205635149083,
+          "rotation": 0,
+          "target": "5-7c"
+        },
+        {
+          "yaw": 1.9853781408069384,
+          "pitch": 1.0990582921688379,
+          "rotation": 0,
+          "target": "6-7d"
+        },
+        {
+          "yaw": 1.0151214946996454,
+          "pitch": 0.9341310019224576,
+          "rotation": 0,
+          "target": "1-2d"
+        },
+        {
+          "yaw": 0.790534187338146,
+          "pitch": 1.2772992680707063,
+          "rotation": 0,
+          "target": "2-4c"
+        },
+        {
+          "yaw": 2.8120103577764697,
+          "pitch": 0.9963358079139439,
+          "rotation": 0,
+          "target": "7-10d"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-2d",
+      "name": "2D",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3600,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-4c",
+      "name": "4C",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3600,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-7a",
+      "name": "7A",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3600,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-7b",
+      "name": "7B",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3600,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-7c",
+      "name": "7C",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3600,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "6-7d",
+      "name": "7D",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3600,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "7-10d",
+      "name": "10D",
       "levels": [
         {
           "tileSize": 256,
@@ -36,11 +324,11 @@ var APP_DATA = {
       "infoHotspots": []
     }
   ],
-  "name": "Project Title",
+  "name": "Estancia Siciliano",
   "settings": {
     "mouseViewMode": "drag",
-    "autorotateEnabled": true,
-    "fullscreenButton": false,
-    "viewControlButtons": false
+    "autorotateEnabled": false,
+    "fullscreenButton": true,
+    "viewControlButtons": true
   }
 };
